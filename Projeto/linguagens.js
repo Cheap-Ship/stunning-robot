e688ab833af62386179a3e5861a454804a6f26eb
Cheap-Ship <pmirmida@gmail.com>
2020-01-30 18:16:25 +0000

    done

---
+++ Projeto/linguagens.js
@@ -0,0 +1,62 @@
+function pt()
+{
+    location.reload();
+}
+
+function en()
+{    
+    //topo
+    document.getElementById("title").innerHTML = "ESMAD's Jornal";
+    document.getElementById("slogan").innerHTML = "If it's important you are the first to know";
+    document.getElementById("regs").innerHTML = "Sign up";
+    document.getElementById("log").innerHTML = "Log in";
+
+    //menu
+    document.getElementById("ref1").innerHTML = "World";
+    document.getElementById("ref2").innerHTML = "Locally";
+    document.getElementById("ref3").innerHTML = "Sports";
+    document.getElementById("ref4").innerHTML = "Politics";
+    document.getElementById("ref5").innerHTML = "Cience";
+    document.getElementById("ref6").innerHTML = "Weather";
+    
+    //aside
+    document.getElementById("ah1").innerHTML = "Rui pinto says there is a lot the portuguese deserve to know";
+    document.getElementById("ap1").innerHTML = "Rui Pinto published a tweet addressed to the Portuguese in the early hours of Thursday, where he claims to be in possession of more confidential information. \"Gold Visas, ESCOM, BES Angola... there is still a lot that the Portuguese deserve to know \", writes the hacker in the Twitter account created in February 2019, as the hacker was under house arrest in Hungary.";
+    
+    document.getElementById("ah2").innerHTML = "SEF fails asylum to Moroccans in December";
+    document.getElementById("ap2").innerHTML = "Dissuades new illegal route, but evaluates newcomers' request The SEF Asylum and Refugees Office gave a negative opinion to the admissibility of the request for international protection of the eight Moroccans who landed in the Algarve in December. Despite this, SEF agreed to consider the asylum application of the 11 who arrived illegally on Tuesday.";
+        
+    document.getElementById("ah3").innerHTML = "European Parliament calls for a common mobile phone charger";
+    document.getElementById("ap3").innerHTML = "How many times have you wanted to charge the iPhone and the colleague next door only has a Samsung charger? Or charge the Huawei and there is only one iPad charger? Brussels has long wanted to end the diversity of chargers and this Thursday, the European Parliament passed a resolution that obliges the committee to introduce mandatory provisions for the introduction of a common charger and, if necessary, to present “a legislative measure until, at the latest, July 2020”.";
+
+    //main
+    document.getElementById("mh1").innerHTML = "Brexit approved in the European parliament";
+    document.getElementById("h1p1").innerHTML = "The European Parliament approved in Wednesday, in Brussels, the Agreement of Exit of the United Kingdom of the European Union, the last formality that lacked for Brexit to materialize next Friday, 31 of January.";
+    document.getElementById("h1p2").innerHTML = "In a vote in which a simple majority of the votes cast was sufficient, the European Parliament “stamped” the UK's exit from the EU with 621 votes in favor, 49 against and 13 abstentions.";
+    document.getElementById("h1p3").innerHTML = "Immediately following the announcement of the result, most of the MEPs who make up the Chamber held hands and started to sing “Auld Lang Syne”, a traditional song based on a poem by Robert Burns, which speaks of the importance of not forgetting old friends, in tribute to the British MPs who officially leave the European Parliament from Friday.";
+    
+    document.getElementById("mh2").innerHTML = "Livre's proposal is \"to provoke reactions more than to do work\", accuses historian Raquel Henriques Silva";
+    document.getElementById("h2p1").innerHTML = "Historian says that Livre was populist with a proposal to return assets from the former colonies, does not generally trust the leaders of other countries and defends the absence of working group activists.";
+    document.getElementById("h2p2").innerHTML = "“Who are we going to return the heritage to?”, Historian Raquel Henriques da Silva asks, after Livre proposed to return cultural goods from the former colonies to their countries of origin. The scientific director of the Museum of Neorealism stressed the importance of knowing the interlocutors of this measure, because, “in general”, the politicians of these countries would not “allow her the slightest sign of confidence”.";
+    document.getElementById("h2p3").innerHTML = "In an interview with Observador radio, the academic began by saying that the proposal has “an excessive political burden” for a cultural theme and accuses the party of being populist. He adds that the \"solitary, provocative and bombastic\" way as presented in the Assembly of the Republic by Joacine Katar Moreira, the party's only deputy, is \"to provoke reactions more than [to] do work\".";
+    
+    document.getElementById("mh3").innerHTML = "Parliament will not vote on Ventura's conviction. Deputy refuses to apologize";
+    document.getElementById("h3p1").innerHTML = "Parliamentary groups and single deputies (with the exception of André Ventura) condemned the words of the deputy of Chega about Joacine, but they do not advance with votes of condemnation for not setting the stage for Ventura.";
+    document.getElementById("h3p2").innerHTML = "The parliamentary leaders decided this morning almost unanimously (only André Ventura was left out) not to proceed with a vote of condemnation in the Parliament to the statements of the deputy of the Chega about the deputy of Livre, Joacine Katar Moreira. Ventura refuses to apologize for what she said on the deputy's facebook and complains that it was an “irony”.";
+    document.getElementById("h3p3").innerHTML = "The condemnation votes that were being weighed by the parties to deputy André Ventura, in the case of the PS for example, were left in the drawer, with PS, PSD, PCP, BE, CDS, Verdes, PAN and single members of IL and Livre to stick the \"repudiation\" of the statements by the deputy of Chega about Joacine Katar Moreira, which the President of the Assembly of the Republic has classified as \"xenophobic\".";
+    
+    document.getElementById("mh4").innerHTML = "Nature needs heroes";
+    document.getElementById("h4p1").innerHTML = "And we need nature, or we are not part of it. In reality, we need to look for ways to coexist peacefully because, in this fight, man will always be the weakest link.";
+    document.getElementById("h4p2").innerHTML = "We are increasingly aware that if we continue to act in the same way as in the past few decades, the world that we will leave for the next generations will not be as habitable, nor as extraordinary in its beauty and diversity. We are increasingly devoting our time and talent to making a difference. We are joining the ranks of heroes fighting for a better future for everyone. Know your superpowers, only to realize that, deep down, we can all be heroes.";
+    document.getElementById("h4p3").innerHTML = "We know that nature has its own strength, but each of us can do our part.";
+
+    //tags
+    document.getElementById("mundo").innerHTML = "#World";
+    document.getElementById("polit").innerHTML = "#Politics";
+    document.getElementById("local").innerHTML = "#Locally";
+    document.getElementById("meteor").innerHTML = "#Weather";
+    document.getElementById("cien").innerHTML = "#Cience";
+
+    //footer
+    document.getElementById("footer").innerHTML = "&copy; ESMAD's Jornal, 2020";
+}
